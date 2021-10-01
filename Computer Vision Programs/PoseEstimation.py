734ec603451a2e05b703e074814c2bc13a484fd6
Surya Sekhar Datta <suryasekhardatta22@gmail.com>
2021-10-01 23:59:48 +0530

    Update PoseEstimation.py

--- Computer Vision Programs/PoseEstimation.py
+++ Computer Vision Programs/PoseEstimation.py
@@ -6,7 +6,7 @@ mpDraw = mp.solutions.drawing_utils
 mpPose = mp.solutions.pose
 pose = mpPose.Pose()
 
-cap = cv2.VideoCapture('PoseVideos/1.mp4')
+cap = cv2.VideoCapture(0)
 pTime = 0
 while True:
     success, img = cap.read()

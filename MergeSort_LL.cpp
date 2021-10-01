4d15865162b46108f206663f1ed723b0b07f9fcc
Divyanshu Singh <55018955+divshacker@users.noreply.github.com>
2021-10-01 21:00:55 +0530

    Update MergeSort_LL.cpp

--- MergeSort_LL.cpp
+++ MergeSort_LL.cpp
@@ -1,13 +1,4 @@
 ```
-/*
-Name: Sreejan Chaudhury
-College: University of Calcutta
-*/
-
-// Merge sort is often preferred for sorting a linked list. The slow random-access performance of a linked list makes some other algorithms (such as quicksort) perform poorly, 
-// and others (such as heapsort) completely impossible.
-
-// C++ code for linked list merged sort 
 #include <bits/stdc++.h> 
 using namespace std; 
   

712ccae3f9dd14a55033ff761c9dba4002822d67
Swapnanil Ray <67406959+redhatpanda@users.noreply.github.com>
2021-10-01 15:00:43 +0530

    Contrib Test

---
+++ live_plot.py
@@ -0,0 +1,48 @@
+import keras
+from IPython.display import clear_output
+import numpy as np
+import matplotlib.pyplot as plt
+
+class LivePlot(keras.callbacks.Callback):
+    
+    # This function is called when the training begins
+    def on_train_begin(self, logs={}):
+        # Initialize the lists for holding the logs, losses and accuracies
+        self.losses = []
+        self.acc = []
+        self.val_losses = []
+        self.val_acc = []
+        self.logs = []
+    
+    # This function is called at the end of each epoch
+    def on_epoch_end(self, epoch, logs={}):
+        
+        # Append the logs, losses and accuracies to the lists
+        self.logs.append(logs)
+        self.losses.append(logs.get('loss'))
+        self.acc.append(logs.get('acc'))
+        self.val_losses.append(logs.get('val_loss'))
+        self.val_acc.append(logs.get('val_acc'))
+        
+        # Before plotting ensure at least 2 epochs have passed
+        if len(self.losses) > 1:
+            
+            # Clear the previous plot
+            clear_output(wait=True)
+            N = np.arange(0, len(self.losses))
+            
+            # You can chose the style of your preference
+            # print(plt.style.available) to see the available options
+            plt.style.use("seaborn")
+            
+            # Plot train loss, train acc, val loss and val acc against epochs passed
+            plt.figure()
+            plt.plot(N, self.losses, label = "train_loss")
+            plt.plot(N, self.acc, label = "train_acc")
+            plt.plot(N, self.val_losses, label = "val_loss")
+            plt.plot(N, self.val_acc, label = "val_acc")
+            plt.title("Training Loss and Accuracy [Epoch {}]".format(epoch))
+            plt.xlabel("Epoch #")
+            plt.ylabel("Loss/Accuracy")
+            plt.legend()
+            plt.show()

e6d50c0b1ba7e26f9bb7d0c5d3e88e052d48f70b
Ruchir Agarwal <91652997+RU200317@users.noreply.github.com>
2021-10-02 12:43:34 +0530

    Create Factorial.py

---
+++ Algorithms/Factorial.py
@@ -0,0 +1,15 @@
+#To get factorial of a given number
+
+# Python 3 program to find
+# factorial of given number
+def factorial(n):
+     
+    # single line to find factorial
+    return 1 if (n==1 or n==0) else n * factorial(n - 1);
+ 
+# Driver Code
+num = 5;
+print("Factorial of",num,"is",
+factorial(num))
+ 
+# This code is contributed by Ruchir Agarwal
